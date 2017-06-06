5f6a9b3ba0685ca7f753166c28b86d8b56e798c8
Veronica K. B. Olsen <code@vkbo.net>
2017-06-06 17:47:37 +0200

    Moved some common database functions to a superclass

---
+++ web/classes/cls_database.php
@@ -0,0 +1,41 @@
+<?php
+   /**
+    *  Where's My Money? – Database Class
+    * ====================================
+    *  Created 2017-06-06
+    */
+
+    class DataBase
+    {
+        // Protected Variables
+        protected $db;
+
+        // Constructor
+        function __construct($oDB) {
+            $this->db = $oDB;
+        }
+
+        // Wrappers
+        protected function dbWrap($dbVar,$varType="text") {
+
+            switch($varType) {
+            case "text":
+                $dbVar = $dbVar === null ? "NULL" : "'".$this->db->real_escape_string($dbVar)."'";
+                break;
+            case "int":
+                $dbVar = $dbVar === null ? "NULL" : intval($dbVar);
+                break;
+            case "float":
+                $dbVar = $dbVar === null ? "NULL" : floatval($dbVar);
+                break;
+            case "date":
+                $dbVar = $dbVar === null ? "NULL" : date("'Y-m-d'",$dbVar);
+                break;
+            case "datetime":
+                $dbVar = $dbVar === null ? "NULL" : date("'Y-m-d H:i:s'",$dbVar);
+                break;
+            }
+            return $dbVar;
+        }
+    }
+?>
---
+++ web/classes/cls_settings.php
@@ -0,0 +1,16 @@
+<?php
+   /**
+    *  Where's My Money? – Settings Class
+    * ====================================
+    *  Created 2017-06-06
+    */
+
+    class Settings extends DataBase
+    {
+        // Constructor
+        function __construct($oDB) {
+            parent::__construct($oDB);
+        }
+
+    }
+?>
